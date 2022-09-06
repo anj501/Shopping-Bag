9b31cd284f9e2ad4f5cc785607f10d235c90cba5
anj501 <110501128+anj501@users.noreply.github.com>
2022-09-06 03:56:30 -0700

    Header file for the Accessories and Gear section

---
+++ aandg.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class aandg {
+    public:
+        void opener(int& total, vector<string>& item, vector<int>& icost);
+        void addtobag(string reply, int& total, vector<string>& item, vector<int>& icost);
+};
