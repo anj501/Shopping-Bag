6676d48ac0e45759f378e0efb801a812c4447034
anj501 <110501128+anj501@users.noreply.github.com>
2022-09-06 03:59:47 -0700

    Header file for jackets and sweaters

---
+++ sandp.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class sandp {
+    public:
+        void opener(int& total, vector<string>& item, vector<int>& icost);
+        void addtobag(string reply, int& total, vector<string>& item, vector<int>& icost);
+
+};
