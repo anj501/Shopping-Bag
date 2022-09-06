a9511a06d79ae6ef6a9bc8a03a28bc095c4c26cd
anj501 <110501128+anj501@users.noreply.github.com>
2022-09-06 03:57:56 -0700

    Header file for jackets and sweaters

---
+++ jands.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class jands {
+    public: 
+        void opener(int& total, vector<string>& item, vector<int>& icost);
+        void addtobag(string reply, int& total, vector<string>& item, vector<int>& icost);
+};
